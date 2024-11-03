d581d1c0211905fd39c0edfe5991eac6d73351b6
Ben Tran <letscontactben@gmail.com>
2024-11-03 15:28:53 +0700

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  res.send("Welcome to CoderSchool");
+router.get("/", (req, res) => {
+  res.send("Welcome to the homepage!");
 });
 
 module.exports = router;
