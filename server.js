80b3bd5e045aa941aa717fa9de5bc546b3eb0e15
Ben Tran <letscontactben@gmail.com>
2024-11-03 15:17:11 +0700

    set up indexroutes

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const pokemonRoutes = require("./routes/pokemons");
+const indexRoutes = require("./routes/index");
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -13,5 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use("/api", pokemonRoutes);
+app.use("/", indexRoutes);
 
 module.exports = app;
