ac3e6ae095086d5de2cf90f72c4466ab26a3d5a5
Ben Tran <letscontactben@gmail.com>
2024-11-03 13:00:09 +0700

    complete be

--- data/pokemonData.js
+++ data/pokemonData.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let pokemonData;
 
-const data = fs.readFileSync("src/data/data.json", "utf8");
+const data = fs.readFileSync("data/data.json", "utf8");
 pokemonData = JSON.parse(data).data;
 
 module.exports = pokemonData;
